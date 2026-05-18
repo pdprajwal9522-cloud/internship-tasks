3b215391bda67bce0fb106cb61447363fd630fe7
P D Prajwal <pdprajwal9522@gmail.com>
2026-05-18 13:55:05 +0530

    designed a box using react

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+  <div className="container">
+    <header className="top-header"><h2>Header</h2></header>
+<div className="mainlayout">
+
+    <div className="leftside">
+    <div className="hero"><h2>Hero</h2></div>
+      <div className="sidebar"><h2>sidebar</h2></div>
+</div>
+    
+    
+    <div className="rightside">
+      
+      <div className="main-content"><h2>Main content</h2></div>
+      <div className="extra-content"><h2>Extra content</h2></div>
+  
+  </div>
+</div>
+<div className="related-box">
+  <div className="related-img"><h2>Related Images</h2></div>
+  <div className="related-posts"><h2>Related Images</h2></div>
+</div>
+<div className="footer"><h2>Footer</h2></div>
+
+
+     </div>
+
+    </>
+  )
+}
+
+export default App
