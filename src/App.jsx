8e639439383d69a0b97dcecb1c43c7c19224ae1b
P D Prajwal <pdprajwal9522@gmail.com>
2026-05-19 12:21:07 +0530

    designing a box

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,14 @@
 
 import './App.css'
+// import Design1 from './components/design1/design1'
+import Design2 from './components/design2/design2'
 
 function App() {
 
   return (
     <>
-
-  <div className="container">
-    <header className="top-header"><h2>Header</h2></header>
-<div className="mainlayout">
-
-    <div className="leftside">
-    <div className="hero"><h2>Hero</h2></div>
-      <div className="sidebar"><h2>sidebar</h2></div>
-</div>
     
-    
-    <div className="rightside">
-      
-      <div className="main-content"><h2>Main content</h2></div>
-      <div className="extra-content"><h2>Extra content</h2></div>
-  
-  </div>
-</div>
-<div className="related-box">
-  <div className="related-img"><h2>Related Images</h2></div>
-  <div className="related-posts"><h2>Related Images</h2></div>
-</div>
-<div className="footer"><h2>Footer</h2></div>
-
-
-     </div>
+    <Design2/>
 
     </>
   )
---
+++ src/components/design2/design2.jsx
@@ -0,0 +1,28 @@
+import './design2.css'
+
+function Design2() {
+
+  return (
+    <>
+<div className="container1">
+<div className="flexboxlayout"><h3>FLEXBOX LAYOUT1</h3></div>
+<div className="header"><h2>Header</h2></div>
+<div className="main-layout">
+  <div className="sidebar"><h2>side bar</h2></div>
+  <div className="contents">
+    <div className="content1"><h2>content 1</h2></div>
+    <div className="content-2-3">
+      <div className="content2"><h2>content-2</h2></div>
+      <div className="content3"><h2>content-3</h2></div>
+    </div>
+  </div>
+</div>
+
+<div className="footer"><h2>Footer</h2></div>
+
+</div>
+    </>
+  )
+}
+
+export default Design2
